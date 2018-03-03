2b5a48558bbc5cc792998dd5e6d9251fb68a4a25
Bennet Gallein <bennet@intranetproject.net>
2018-03-03 22:13:42 +0100

    Added docs

--- src/de/bennetgallein/neuron/Input.java
+++ src/de/bennetgallein/neuron/Input.java
@@ -1,10 +1,26 @@
 package de.bennetgallein.neuron;
 
+/**
+ * The Input class
+ * @author Bennet
+ *
+ */
 public class Input {
-
+	
+	/**
+	 * The weight of the input
+	 */
 	float w;
+	/**
+	 * The value of the input
+	 */
 	float x;
 
+	/**
+	 * Constructor for the Input
+	 * @param w the weight of the Input
+	 * @param x the value of the Input
+	 */
 	public Input(float w, float x) {
 		this.w = w;
 		this.x = x;
--- src/de/bennetgallein/neuron/Neuron.java
+++ src/de/bennetgallein/neuron/Neuron.java
@@ -2,16 +2,37 @@ package de.bennetgallein.neuron;
 
 import java.util.ArrayList;
 
+/**
+ * The Neuron class
+ * @author Bennet
+ *
+ */
 public class Neuron {
-
+	
+	/**
+	 * Input Array for the Neuron 
+	 */
 	ArrayList<Input> i;
+	
+	/**
+	 * the bias (negative of the threshold
+	 */
 	float b;
 	
+	/**
+	 * 
+	 * @param input The Input objects in a ArrayList
+	 * @param b the bias of the Neuron
+	 */
 	public Neuron(ArrayList<Input> input, float b) {
 		this.i = input;
 		this.b = b;
 	}
-
+	
+	/**
+	 * This returns the output of the Neuron
+	 * @return 0 or 1 depending if the output is bigger than 0 or smaller.
+	 */
 	public float fire() {
 		float sum = 0;
 		for (int z = 0; z < this.i.size(); z++) {
@@ -25,6 +46,10 @@ public class Neuron {
 			return 1;
 		}
 	}
+	/**
+	 * This returns the sigmoid function value of the output of the Neuron
+	 * @return a float depending on the output
+	 */
 	public double fireSigmoid() {
 		double sum = 0;
 		for (int z = 0; z < this.i.size(); z++) {
